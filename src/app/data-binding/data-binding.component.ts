c929114056804cfd902258effdacaee258658142
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-07-03 15:02:24 -0300

    Ta na metade

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -9,13 +9,30 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/sports';
-
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
   getValor() {
     return +1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() {}
